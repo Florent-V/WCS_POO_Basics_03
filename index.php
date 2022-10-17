93ec33d8e63113c722a92e443c0c1d4bc8d75e34
Florent-Vasseur <vasseurflorent@gmail.com>
2022-10-17 10:26:38 +0200

    ajout code fichier index.php pour test

--- index.php
+++ index.php
@@ -1,10 +1,23 @@
 <?php 
-
+require_once './Vehicle.php';
 require_once './HighWay.php';
 require_once './MotorWay.php';
 
 
+
+
+$car1 = new Vehicle('blue', 4);
+$car2 = new Vehicle('red', 5);
+
 $a4 = new MotorWay();
+
+var_dump($a4);
+
+$a4->addVehicle($car1);
+
+var_dump($a4);
+
+$a4->addVehicle($car2);
 
 var_dump($a4);
 
