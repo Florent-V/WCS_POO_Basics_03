82761f14304eb9d274b1cf5d71a2b218a5d21af8
Florent-Vasseur <vasseurflorent@gmail.com>
2022-10-17 10:15:57 +0200

    erreur typo corr

--- MotorWay.php
+++ MotorWay.php
@@ -4,8 +4,11 @@ require_once './HighWay.php';
 
 final class MotorWay extends HighWay
 {
-    protected int $nbLane = 4;
-    protected int $maxSpeed = 130; 
+    
+    
+    protected int $nbLane=4;
+    protected int $maxSpeed=130;
+
 
     public function addVehicle(Vehicle $vehicle) {
         if (!($vehicle instanceof Bike) && !($vehicle instanceof Skateboard)) {
--- index.php
+++ index.php
@@ -1,9 +1,12 @@
 <?php 
 
 require_once './HighWay.php';
+require_once './MotorWay.php';
 
 
+$a4 = new MotorWay();
 
+var_dump($a4);
 
 
 ?>
